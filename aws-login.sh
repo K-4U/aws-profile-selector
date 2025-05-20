e82aa66286574d661f49c8f083dd3e842c499190
Koen Beckers <koen@k-4u.nl>
2025-05-20 22:30:53 +0200

    Tweaked the bash script to work from PATH.

--- aws-login.sh
+++ aws-login.sh
@@ -1,5 +1,13 @@
+#!/usr/bin/env bash
+if [[ "$0" == */* ]]; then
+  SCRIPT_PATH="$0"
+else
+  SCRIPT_PATH="$(command -v "$0")"
+fi
+SCRIPT_PATH="$(python3 -c 'import os,sys; print(os.path.realpath(sys.argv[1]))' "$SCRIPT_PATH")"
+SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
 TEMP=$(mktemp)
-python3 main.py "$TEMP"
+python3 "$SCRIPT_DIR/main.py" "$TEMP"
 
 source "$TEMP"
 rm "$TEMP"
